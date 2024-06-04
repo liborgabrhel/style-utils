e198ad7b080dd9bb3fa5aae29c746539dcc6975f
Libor Gabrhel <gabrhel.libor@gmail.com>
2024-06-04 18:02:22 +0200

    feat: added support for undefined class value

--- test/index.test.ts
+++ test/index.test.ts
@@ -10,6 +10,10 @@ test("combineClasses should filter out empty classes", () => {
   assert.equal(combineClasses("class1", "", "class2"), "class1 class2")
 })
 
+test("combineClasses should filter out undefined classes", () => {
+  assert.equal(combineClasses("class1", undefined, "class2"), "class1 class2")
+})
+
 test("applyClasses should apply classes conditionally", () => {
   assert.equal(applyClasses("class1", "class2").if(true), "class1 class2")
   assert.equal(applyClasses("class1", "class2").if(false), "")
